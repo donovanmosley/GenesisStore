d06ff4be80b4b2741dab28836f013aae1b5a228e
Donovan Mosley <dmosley28@gmail.com>
2020-01-14 05:30:12 -0500

    first commit

---
+++ src/components/Context.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+
+const ProductContext = React.createContext();
+//Every React.reateContext object comes with 2 components:
+//Provider - Provides all information for the application
+//Consmer - 
+
+class ProductProvider extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
