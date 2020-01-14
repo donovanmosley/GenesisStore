e148ed4927621ab95c5bad3d4849c0572f26a8e8
Donovan Mosley <dmosley28@gmail.com>
2020-01-14 06:15:22 -0500

    consumer explained

--- src/components/Context.js
+++ src/components/Context.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react'
 const ProductContext = React.createContext();
 //Every React.reateContext object comes with 2 components:
 //Provider - Provides all information for the application
-//Consmer - 
+//Consmer - Uses the information from the consumer
 
 class ProductProvider extends Component {
     render() {
